e1aa3844bae182743673abed77c920ad20119931
jcangeles18 <107091660+jcangeles18@users.noreply.github.com>
2022-11-11 13:50:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const navSlide =() => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('nav-active');
+    
+        navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+                link.style.animation = '';
+        } else {
+            link.style.animation = 'navLinkFade 1.5s ease forwards ${index / 7 + 0.5}s';
+        }
+     });
+
+     burger.classList.toggle('toggle');
+
+    });
+
+}
+ 
+navSlide();
+
+
+//  rotating banner
+		  var myIndex = 0;
+		  carousel();
+		  
+		  function carousel() {
+			var i;
+			var x = document.getElementsByClassName("mySlides");
+			for (i = 0; i < x.length; i++) {
+			  x[i].style.display = "none";  
+			}
+			myIndex++;
+			if (myIndex > x.length) {myIndex = 1}    
+			x[myIndex-1].style.display = "block";  
+			setTimeout(carousel, 3000);    
+		  }
+
+
